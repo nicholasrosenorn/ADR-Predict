785e53bcb0d0ca4882eadbce1ee68c3de794d883
szapodea <szapodea@purdue.edu>
2023-10-16 20:08:26 -0400

    dropped constant dimensions

--- preprocessing/preprocessing.py
+++ preprocessing/preprocessing.py
@@ -2,11 +2,18 @@ import pandas as pd
 import numpy as np
 
 
-# TODO: implement pre-processing functions
+# function that looks at all columns and checks if they have one value assigned to it.
+# drops all such columns
+def drop_cols(data):
+    empty_cols = []
+    for col in data.columns:
+        if len(data[col].unique()) <= 1:
+            #print(col, data[col].unique())
+            empty_cols.append(col)
 
+    data.drop(labels=empty_cols, axis=1, inplace=True)
 
-
-
+    return data
 
 
 
@@ -14,4 +21,9 @@ import numpy as np
 
 if __name__ == '__main__':
     # Main function
+    data = pd.read_csv('../data/merged.csv')
+    dropped_col_data = drop_cols(data=data)
+
+    # commented out for now
+    #pd.to_csv('../data/dropped_col.csv')
 
